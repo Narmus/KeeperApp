e28593b5a3fb3c2df82cc00808e8cfc13e49357a
Arun M S <narmusgaming@gmail.com>
2022-10-08 10:53:48 +0530

    Created Keeper App Stylings

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const time = new Date();
+const year = time.getFullYear();
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright {year}</p>
+    </footer>
+  );
+}
+
+export default Footer;
