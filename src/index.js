4f8964b647bffd27b2c9e7785be1f409c2ef4187
Arun M S <narmusgaming@gmail.com>
2022-10-30 18:08:57 +0530

    removed google fonts temporarily due to deploy error

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
-// const root = ReactDOM.createRoot();
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 //CHALLENGE:
