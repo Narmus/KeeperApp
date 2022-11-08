e3ade46815f9fd70b0286127a44b3db43dbea8e6
Arun M S <narmusgaming@gmail.com>
2022-11-08 21:58:42 +0530

    Updates

---
+++ precache-manifest.7ede26c53d0bf8faee9beb450a3c43a3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ac7fa8d612ee7ea1514a44b0a9dbc9",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "28e7240934e05d075d4e",
+    "url": "/KeeperApp/static/js/2.8c62a5fe.chunk.js"
+  },
+  {
+    "revision": "e8e79a127b97b38eba86",
+    "url": "/KeeperApp/static/js/main.162766b8.chunk.js"
+  },
+  {
+    "revision": "25ce74ec801985d27893",
+    "url": "/KeeperApp/static/js/runtime-main.7621dc85.js"
+  }
+]);
